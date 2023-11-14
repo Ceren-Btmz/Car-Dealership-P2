e427db2434a1778004b24ad769f88c0e4a2e26b8
Crn-Btmz <Ceren.btmz@gmail.com>
2023-11-13 22:32:29 -0500

    added base info + contract class

---
+++ src/main/java/com/pluralsight/SalesContract.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class SalesContract extends Contract {
+
+    private double recordingFee;
+    private double processingFee;
+    private boolean finance;
+
+
+
+
+}
